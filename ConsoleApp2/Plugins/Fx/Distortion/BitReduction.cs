f9ec908a9e474e51e1c0237e49e949cd88ccf567
Koby Levy <86796339+KobyLevy512@users.noreply.github.com>
2024-02-23 20:33:51 +0200

    Added Bit Reduction plugin.

---
+++ ConsoleApp2/Plugins/Fx/Distortion/BitReduction.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ConsoleApp2.Plugins.Fx.Distortion
+{
+    public class BitReduction : PluginBase
+    {
+        public double Mix;
+        ushort depth;
+        public ushort Depth
+        {
+            get => depth;
+            set
+            {
+                depth = (ushort)(ushort.MaxValue - ushort.MaxValue * value);
+            }
+        }
+        public override void Process(ref double l, ref double r)
+        {
+            if(GetChannel().BelongMixer.SamplePosition % depth == 0)
+            {
+                l = l - l * Mix;
+                r = r - r * Mix;
+            }
+        }
+    }
+}
