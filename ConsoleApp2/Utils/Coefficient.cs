65b62cbc4d4fbf6b6634c8acd66227aa08132433
Koby Levy <86796339+KobyLevy512@users.noreply.github.com>
2024-02-28 22:24:20 +0200

    Coefficient - Add constructor from double array

--- ConsoleApp2/Utils/Coefficient.cs
+++ ConsoleApp2/Utils/Coefficient.cs
@@ -18,6 +18,25 @@ namespace ConsoleApp2.Utils
             memInputs = new double[(int)Math.Ceiling(half)];
             memOutputs = new double[(int)Math.Ceiling(half)];
         }
+        public Coefficient(double[] arr)
+        {
+            int size = arr.Length;
+            if (size < 2) throw new ArgumentException("Size must be bigger than 1.");
+            float half = size / 2.0f;
+            coefInput = new double[(int)Math.Ceiling(half)];
+            coefOutput = new double[(int)half];
+            half--;
+            memInputs = new double[(int)Math.Ceiling(half)];
+            memOutputs = new double[(int)Math.Ceiling(half)];
+
+            int index = 0;
+            for(int i= 0; i< coefInput.Length; i++)
+            {
+                coefInput[i] = arr[index++];
+                if(i < coefOutput.Length)
+                    coefOutput[i] = arr[index++];
+            }
+        }
 
         public (double input, double output) this[int x, int y]
         {
@@ -39,6 +58,26 @@ namespace ConsoleApp2.Utils
             {
                 coefOutput[i] = src.coefOutput[i];
             }
+        }
+        /// <summary>
+        /// Linear interpolate between 2 coefficient.
+        /// </summary>
+        /// <param name="l"></param>
+        /// <param name="r"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Coefficient Lerp(Coefficient l, Coefficient r, double value)
+        {
+            Coefficient ret = new Coefficient(l.coefInput.Length + l.coefOutput.Length);
+            for (int i = 0; i < ret.coefInput.Length; i++)
+            {
+                ret[i, 0] = (EMath.Lerp(l.coefInput[0], r.coefInput[0], value), 0);
+            }
+            for (int i = 0; i < ret.coefOutput.Length; i++)
+            {
+                ret[0, i] = (ret[0, 0].input, EMath.Lerp(l.coefOutput[0], r.coefOutput[0], value);
+            }
+            return ret;
         }
         public override void Process(ref double mono)
         {
